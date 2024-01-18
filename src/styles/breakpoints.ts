76c171085e64e802058dbfb841f2d9a7256d05c1
Tobias Wiklund <tobias.wiklund@stampen.com>
2024-01-18 23:51:05 +0100

    add breakpoints

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,7 @@
+export const device = {
+  xs: 'screen and (min-width: 0px)',
+  sm: 'screen and (min-width: 600px)',
+  md: 'screen and (min-width: 900px)',
+  lg: 'screen and (min-width: 1200px)',
+  xl: 'screen and (min-width: 1536px)',
+}
